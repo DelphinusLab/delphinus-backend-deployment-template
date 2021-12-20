23abb036f097448d6afffdcddac5b9b1b6ca1bae
zoyoe <zkp@zoyoe.com>
2021-12-20 23:08:26 +0800

    remove fs

--- src/remote/eth-config.ts
+++ src/remote/eth-config.ts
@@ -1,14 +1,8 @@
 import CryptoENC from "crypto-js/enc-utf8";
-import * as fs from "fs";
 import { L1ClientRole, ChainConfig } from "../types";
 
 const cryptoJS = require("crypto-js");
 const fetch = require("node-fetch");
-
-function decrypt(data: string) {
-  let prikey = fs.readFileSync("prikey").toString();
-  return cryptoJS.AES.decrypt(data, prikey).toString(CryptoENC);
-}
 
 export async function queryEthConfig(
   role: L1ClientRole,
@@ -23,5 +17,5 @@ export async function queryEthConfig(
       uri = apiUri + "/eth-config/wallet";
   }
   let text = await (await fetch(uri)).text();
-  return JSON.parse(decrypt(text)).chains;
+  return JSON.parse(text).chains;
 }
