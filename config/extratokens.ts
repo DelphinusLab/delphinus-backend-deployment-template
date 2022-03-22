4e733efb29828480824b6df10a1e449adef53207
sinka <xgao@zoyoe.com>
2022-03-22 11:30:51 +1100

    add extratokens for testnet

---
+++ config/extratokens.ts
@@ -0,0 +1,22 @@
+export const extraTokens = [
+  {
+    chainId: "97",
+    name: "usdt",
+    address: "337610d27c682E347C9cD60BD4b3b107C9d34dDd"
+  },
+  {
+    chainId: "97",
+    name: "usdc",
+    address: "813b24C0f3BafEB03DB596B97D487ac362756961"
+  },
+  {
+    chainId: "3",
+    name: "usdt",
+    address:"6EE856Ae55B6E1A249f04cd3b947141bc146273c"
+  },
+  {
+    chainId: "3",
+    name: "usdc",
+    address: "FE724a829fdF12F7012365dB98730EEe33742ea2"
+  }
+]
