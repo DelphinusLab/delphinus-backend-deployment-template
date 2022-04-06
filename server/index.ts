e08f4659b92fe96eee5d383fd4de62cccd186958
Xin Gao <xgao@zoyoe.com>
2022-04-06 17:48:30 +1000

    add db rest api

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,20 @@
 import express from "express";
-// import * as fs from "fs";
 
 import serverConf from "../config/server.json";
 import substrateNode from "../config/substrate-node.json";
 import merkleTreeConfig from "../config/merkle-tree-config.json";
 import { ethConfigbyRole } from "../config/eth-config";
 import { L1ClientRole } from "../src/types";
+import { DBClient } from './db';
+
+const BridgeJSON = require("solidity/build/contracts/Bridge.json");
 
 const app = express();
 const port = serverConf.port;
-/*
-const cryptoJS = require("crypto-js");
-
-function encrypt(data: string) {
-  let prikey = fs.readFileSync("prikey").toString();
-  return cryptoJS.AES.encrypt(data, prikey).toString();
-}
-*/
 
 function main() {
+  const client = new (DBClient);
+
   app.get("/substrate-node", (req, res) => {
     res.send(JSON.stringify(substrateNode));
   });
@@ -37,6 +33,21 @@ function main() {
 
   app.listen(serverConf.port, () => {
     console.log(`server started at http://localhost:${port}`);
+  });
+
+  app.get('/substrate/:collection_name', async (req:any, res:any) => {
+    client.getAll('substrate', req.params.collection_name).then((result:any) => {
+      res.send(result);
+    })
+  });
+
+  app.get('/eth/:eid/:ename/', async (req:any, res:any) => {
+    const network_id = req.params.eid;
+    const address = BridgeJSON.networks[network_id].address;
+    const db_name = network_id + address;
+    client.getAll(db_name, req.params.ename).then((result:any) => {
+      res.send(result);
+    })
   });
 }
 
