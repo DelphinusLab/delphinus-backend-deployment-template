a778b9e2f5d1d8128b6ff44cf5fa17bd265dd3e4
sinka <xgao@zoyoe.com>
2022-05-14 17:55:40 +1000

    add wei to extra tokens' info

--- config/extratokens.ts
+++ config/extratokens.ts
@@ -2,21 +2,25 @@ export const extraTokens = [
   {
     chainId: "97",
     name: "usdt",
+    wei: 6,
     address: "337610d27c682E347C9cD60BD4b3b107C9d34dDd"
   },
   {
     chainId: "97",
     name: "usdc",
+    wei: 6,
     address: "813b24C0f3BafEB03DB596B97D487ac362756961"
   },
   {
     chainId: "3",
     name: "usdt",
-    address:"110a13FC3efE6A245B50102D2d79B3E76125Ae83"
+    wei: 6,
+    address: "110a13FC3efE6A245B50102D2d79B3E76125Ae83"
   },
   {
     chainId: "3",
     name: "usdc",
+    wei: 6,
     address: "07865c6E87B9F70255377e024ace6630C1Eaa37F"
   }
 ]
