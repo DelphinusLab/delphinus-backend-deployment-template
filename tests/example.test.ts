85edab54fe01e47850aacb6a0f59f0a52f889566
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-10-11 13:05:10 +1100

    modify example tests

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,11 +1,11 @@
-import { getConfigByChainName } from "../src/config";
+import { getConfigByChainName, getConfigByChainId } from "../src/config";
 import { L1ClientRole } from "../src/types";
 
-describe("test getConfigByChainName works", () => {
-    test("test if it throw error correctly", async () => {
+describe("test get config functions", () => {
+    test("test if getConfigByChainName throw error correctly", async () => {
         jest.setTimeout(60000); //1 minute timeout
         try{
-            await getConfigByChainName(L1ClientRole.Monitor,"opsten");
+            await getConfigByChainName(L1ClientRole.Monitor,"sctestnet");
         }catch(err){
             if (err instanceof Error) {
                 expect(err.message).toEqual("chain name is not matched.");
@@ -15,9 +15,28 @@ describe("test getConfigByChainName works", () => {
         }
     });
 
-    test("test if works correctly", async () => {
+    test("test if getConfigByChainName works correctly", async () => {
         jest.setTimeout(60000); //1 minute timeout
-        const config = await getConfigByChainName(L1ClientRole.Monitor,"ropsten");
-        expect(config.deviceId).toEqual("3");
+        const config = await getConfigByChainName(L1ClientRole.Monitor,"bsctestnet");
+        expect(config.deviceId).toEqual("97");
+    });
+
+    test("test if getConfigByChainId throw error correctly", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        try{
+            await getConfigByChainId(L1ClientRole.Monitor,"98");
+        }catch(err){
+            if (err instanceof Error) {
+                expect(err.message).toEqual("chain id is not matched.");
+            }else{
+                console.log('Unexpected error', err);
+            }
+        }
+    });
+
+    test("test if getConfigByChainId works correctly", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        const config = await getConfigByChainId(L1ClientRole.Monitor,"97");
+        expect(config.chainName).toEqual("bsctestnet");
     });
 });
