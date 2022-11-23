d097919f540c22d2db2812e7d0b249f1a30d5e2f
Richard Hao <richard@rhao.io>
2022-11-23 10:57:21 +0000

    add error handling to server

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 const port = serverConf.port;
 
-function main() {
+async function main() {
   const client = new DBClient();
 
   app.get("/substrate-node", (req, res) => {
@@ -35,56 +35,72 @@ function main() {
     console.log(`server started at http://localhost:${port}`);
   });
 
-  app.get("/substrate/:collection_name", async (req: any, res: any) => {
-    client
-      .getAll("substrate", req.params.collection_name)
-      .then((result: any) => {
+  app.get(
+    "/substrate/:collection_name",
+    async (req: any, res: any, next: any) => {
+      try {
+        client
+          .getAll("substrate", req.params.collection_name)
+          .then((result: any) => {
+            res.send(result);
+          });
+      } catch (e: any) {
+        next(e);
+      }
+    }
+  );
+
+  app.get("/l2event/:start/:length", async (req: any, res: any, next: any) => {
+    try {
+      client
+        .getRange(
+          "substrate",
+          "l2_event",
+          parseInt(req.params.start),
+          parseInt(req.params.length)
+        )
+        .then((result: any) => {
+          res.status(200).send({ success: true, result: result });
+        });
+    } catch (e: any) {
+      next(e); // Pass to error handler
+    }
+  });
+  app.get("/l2transactions", async (req: any, res: any, next: any) => {
+    try {
+      client.getAll("substrate", "l2_transactions").then((result: any) => {
         res.send(result);
       });
-  });
-
-  app.get("/l2event/:start/:length", async (req: any, res: any) => {
-    client
-      .getRange(
-        "substrate",
-        "l2_event",
-        parseInt(req.params.start),
-        parseInt(req.params.length)
-      )
-      .then((result: any) => {
-        res.status(200).send({ success: true, result: result });
-      });
-  });
-  app.get("/l2transactions", async (req: any, res: any) => {
-    // client.getRange('substrate', "l2_transactions", parseInt(req.params.start), parseInt(req.params.length)).then((result:any) => {
-    //   res.status(200).send({"success": true, result: result});
-    // });
-    client.getAll("substrate", "l2_transactions").then((result: any) => {
-      res.send(result);
-    });
+    } catch (e: any) {
+      next(e); // Pass to error handler
+    }
   });
   app.get(
     "/l2transactions/:l2Address/:l2AccountIndex",
-    async (req: any, res: any) => {
-      // client.getRange('substrate', "l2_transactions", parseInt(req.params.start), parseInt(req.params.length)).then((result:any) => {
-      //   res.status(200).send({"success": true, result: result});
-      // });
-      const { l2Address, l2AccountIndex } = req.params;
-      console.log("l2Address", l2Address);
-      console.log("l2AccountIndex", l2AccountIndex);
-      let txs = await client.invokeDB(async (client: any) => {
-        const db = client.db("substrate");
-        return await db
-          .collection("l2_transactions")
-          .find({
-            $or: [{ signer: l2Address }, { accountIndex: l2AccountIndex }],
-          })
-          .sort({ timestamp: -1 })
-          .toArray();
-      });
-      res.send(txs);
+    async (req: any, res: any, next: any) => {
+      try {
+        const { l2Address, l2AccountIndex } = req.params;
+        console.log("l2Address", l2Address);
+        console.log("l2AccountIndex", l2AccountIndex);
+        let txs = await client.invokeDB(async (client: any) => {
+          const db = client.db("substrate");
+          return await db
+            .collection("l2_transactions")
+            .find({
+              $or: [{ signer: l2Address }, { accountIndex: l2AccountIndex }],
+            })
+            .sort({ timestamp: -1 })
+            .toArray();
+        });
+        res.send(txs);
+      } catch (e: any) {
+        next(e); // Pass to error handler
+      }
     }
   );
+
+  //Generalised error handler
+  app.use(errorHandler);
 
   /* Layer1 event track
   app.get('/eth/:eid/:ename/', async (req:any, res:any) => {
@@ -99,4 +115,9 @@ function main() {
   */
 }
 
+//error handler function, pass it to app.use, log any issues and send a 500 response
+function errorHandler(err: any, req: any, res: any, next: any) {
+  console.error(err);
+  res.status(500).send("Internal server error");
+}
 main();
